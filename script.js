597e777b7fd86eb1cceed70bf5867fac096df9b9
Pedro_Dourado26 <pedro.dourado@uscsonline.com.br>
2026-03-17 21:59:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Criação Mini Sistema de identificação de Cliente para loja em Javascript
 
 let cliente = "Marcos";
-let produto = "ControleBranco";
-const preco = 600.00;
-const valorTotal = Soma;
+let produto1 = "ControlesBranco";
+let produto2 = "Memorias"
+const preco1 = "R$600.00";
+const preco2 = "R$300.00";
 const quantidade = 2;
 let atendente = "Mariana";
 
 console.log(cliente);
+console.log(produto1);
+console.log(atendente);
+console.log(preco1);
+console.log(preco2);
+console.log(quantidade);
+console.log(produto2);
+
+let valor1 = 600.00;
+let valor2 = 2;
+let valor3 = 300.00;
+let valor4 = 2;
+
+console.log(valor1 * valor2); 
+console.log(valor3 * valor4);
+
+console.log(`O ${cliente} comprou dois ${produto1} e também duas ${produto2} pelo valor de ${preco1} e ${preco2}
+    respectivamente. O ${cliente} foi atendido pela ${atendente}`);
