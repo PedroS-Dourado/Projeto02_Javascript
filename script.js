bb455a198ec02120da47d344398df61825d869c2
Pedro_Dourado26 <pedro.dourado@uscsonline.com.br>
2026-03-17 21:09:57 -0300

    feat: atividade inicial de sintaxe js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Criação Mini Sistema de identificação de Cliente para loja em Javascript
+
+let cliente = "Marcos";
+let produto = "ControleBranco";
+const preco = 600.00;
+const valorTotal = Soma;
+const quantidade = 2;
+let atendente = "Mariana";
+
+console.log(cliente);
